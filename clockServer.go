8ab1e24a19b38730fc0a47f64c3ef0a3faf948b5
Marlene Harms <marlene_harms@hotmail.com>
2022-05-11 02:04:08 -0500

    solved lab

--- clockServer.go
+++ clockServer.go
@@ -1,31 +1,45 @@
-// Clock Server is a concurrent TCP server that periodically writes the time.
+// Clock2 is a concurrent TCP server that periodically writes the time.
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
 func handleConn(c net.Conn) {
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n")) // write the time
 		if err != nil {
 			return // e.g., client disconnected
 		}
-		time.Sleep(1 * time.Second)
+		time.Sleep(1 * time.Second) // wait 1s
 	}
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+	if len(os.Args) < 3 { // check if the number of arguments is correct
+		fmt.Println("Invlid number of arguments.")
+		return
+	}
+
+	if os.Args[1] != "-port" { // check if the first argument is -port
+		fmt.Println("Invalid flag.")
+		return
+	}
+
+	// Listen on TCP port and run on argument 2
+	l, err := net.Listen("tcp", "localhost:"+os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for {
-		conn, err := listener.Accept()
+		conn, err := l.Accept()
 		if err != nil {
 			log.Print(err) // e.g., connection aborted
 			continue
--- clockWall.go
+++ clockWall.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func printTime(dst io.Writer, src io.Reader, city string) { // print the time dsr is the destination, src is the source, city is the city name
+	s := bufio.NewScanner(src) // create a scanner
+	for s.Scan() {             // scan the time
+		fmt.Printf("%s\t: %s\n", city, s.Text()) // print the time
+	}
+}
+
 func main() {
-	// Let's start the fun
+	for i := 1; i < len(os.Args); i++ {
+		arr := strings.Split(os.Args[i], "=") // split the argument
+
+		if len(arr) != 2 { // check if the number of arguments is correct
+			fmt.Println("Invalid format for arguments. Format: <cityName>=<portNumber>")
+			return
+		}
+
+		conn, err := net.Dial("tcp", arr[1]) // connect to the port
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer conn.Close() // close the connection when the function ends
+
+		go printTime(os.Stdout, conn, arr[0]) // print the time concurrently
+	}
+
+	ch := make(chan int) // create a channel
+	<-ch                 // wait for the channel to be closed
 }
